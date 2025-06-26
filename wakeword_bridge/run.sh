d2c2e3bca7e6377c9f21700a21d125b38b7d94c0
MaverickKB <kbandoly@ysnetron.com>
2025-06-26 11:36:45 -0400

    updated for HA2025.6.3 (hopefully the last one)

--- wakeword_bridge/run.sh
+++ wakeword_bridge/run.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bashio
-bashio::log.info "Starting Wake Word Bridge..."
-# Read options: e.g., PORT=$(bashio::config 'port')
+#!/usr/bin/with-contenv bashio
+bashio::log.info "Starting Wake Word Bridge…"
+bashio::config # to pull options
 python3 /udp_to_wyoming_bridge.py
