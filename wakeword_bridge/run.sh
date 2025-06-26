77b2b53a91aec1a11fe3bd49c26b85c48d84a12c
MaverickKB <kbandoly@synetron.com>
2025-06-26 13:45:54 -0400

    should be good now, right?

--- wakeword_bridge/run.sh
+++ wakeword_bridge/run.sh
@@ -1,4 +1,16 @@
-#!/usr/bin/with-contenv bashio
-bashio::log.info "Starting Wake Word Bridge…"
-bashio::config # to pull options
-python3 /udp_to_wyoming_bridge.py
+#!/usr/bin/env bash
+set -e
+
+# shellcheck disable=SC1091
+source /usr/lib/bashio/bashio.sh
+
+MQTT_HOST=$(bashio::config 'mqtt_host')
+MQTT_PORT=$(bashio::config 'mqtt_port')
+MQTT_USER=$(bashio::config 'mqtt_username')
+MQTT_PASS=$(bashio::config 'mqtt_password')
+SITE_ID=$(bashio::config 'site_id')
+
+exec python3 /udp_to_wyoming_bridge.py \
+     --host "$MQTT_HOST" --port "$MQTT_PORT" \
+     --username "$MQTT_USER" --password "$MQTT_PASS" \
+     --site-id "$SITE_ID"
