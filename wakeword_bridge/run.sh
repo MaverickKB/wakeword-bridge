488298dbdf9452bcf261ca578798d1d9addb425a
MaverickKB <kbandoly@synetron.com>
2025-06-26 11:13:45 -0400

    Update run.sh

--- wakeword_bridge/run.sh
+++ wakeword_bridge/run.sh
@@ -1,13 +1,4 @@
-#!/usr/bin/env bash
-
-# Load bashio config parser
-source /usr/lib/bashio/bashio.sh
-
-export MQTT_HOST=$(bashio::config 'mqtt_host')
-export MQTT_PORT=$(bashio::config 'mqtt_port')
-export MQTT_USERNAME=$(bashio::config 'mqtt_username')
-export MQTT_PASSWORD=$(bashio::config 'mqtt_password')
-export SITE_ID=$(bashio::config 'site_id')
-
-echo "Starting Wake Word Bridge for site: $SITE_ID"
+#!/usr/bin/env bashio
+bashio::log.info "Starting Wake Word Bridge..."
+# Read options: e.g., PORT=$(bashio::config 'port')
 python3 /udp_to_wyoming_bridge.py
